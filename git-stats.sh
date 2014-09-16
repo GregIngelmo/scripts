aaddd89ccb7fbe9e2de7044c99e655c22c0f4dda
Greg Ingelmo <git@ingelmo.org>
2014-09-15 22:07:53 -0400

    Cosmetic changes

--- git-stats.sh
+++ git-stats.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Script to show that local and remote stats for current git repo
+# Script to show the local and remote stats for current git repo
 # useful for running in terminal pane alongside your code
 # To keep this updated as you edit your code run
 # fswatch --exclude "\.git" -o . | xargs -n1 -I {} git-stats.sh
 
 clear && \
 git fetch --quiet && \
-echo 'Unstaged changes' && \
+echo "Unstaged changes" && \
 git --no-pager diff --stat && \
 echo "\nLocal commits" && \
 git --no-pager log --pretty=format:"- %s (%cn)" origin/HEAD..HEAD && \
-echo "\nRemote commits" && \
+echo "\n\nRemote commits" && \
 git --no-pager log --pretty=format:'- %s (%cn, %ar)' HEAD..origin/HEAD
